99cc5144cb689b53cd1d5a56c68141a9ecca4602
XilongWang <3382272471@qq.com>
2025-03-24 21:57:32 +0800

    test: :white_check_mark: an simple test for cz-git
    cz test

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="title">
-    <h2>测试</h2>
+    <h2>测试页</h2>
     <el-button @click="fetchOverviewData">获取用户信息</el-button>
     <el-button @click="fetchArticleData">获取文章列表</el-button>
     <el-button @click="fetchLabelData">获取标签列表</el-button>
